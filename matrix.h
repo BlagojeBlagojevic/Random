5826662e83caf28156a4c4cfbd9ca6a87cf27cff
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-02-11 09:14:29 +0100

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -6,20 +6,22 @@
 #include<stdint.h>
 
 #ifndef MATRIX_CALLOC
-#include<stdlib.h> 
+#include<stdlib.h>
 #define MATRIX_CALLOC calloc
 #endif
 
 #ifndef MATRIX_ASSERT
-#include<assert.h> 
+#include<assert.h>
 #define MATRIX_ASSERT assert
 #endif
 
 #define MATRIX_SHIFT(m,y,x) (m).elem[(y)*(m).cols + (x)]
 
-#ifndef TYPE 
+#ifndef TYPE
 #define TYPE float
 #endif
+
+
 
 typedef struct {
 	size_t rows;    //NUM OF   ROWS
@@ -30,85 +32,126 @@ typedef struct {
 
 //UTILITY FUNCTION
 float rand_float(void);
+float sigmoid(float x);
 
 //MATRIX OPERATION
-Mat matrix_alloc(size_t rows, size_t cols);
+Mat  matrix_alloc(size_t rows, size_t cols);
 void matrix_dot(Mat dest, Mat a, Mat b);
 void matrix_sum(Mat dest, Mat a);
 void matrix_rand(Mat dest,float low, float high);
 void matrix_print(Mat m,const char *name);
-#define MATRIX_PRINT(m) matrix_print(m, #m)
+void matrix_activation_cols(Mat m,size_t num);
+void matrix_feedforward(Mat *out,Mat input, Mat *weights, Mat *bias, size_t num_of_layer);
+#define MATRIX_PRINT(m) matrix_print((m), #m)
 
 #endif  //MATRIX_H
 
 
 #ifdef MATRIX_IMPLEMETATION
-float rand_float(void){
+float rand_float(void) {
 	return (float)rand() / (float)RAND_MAX;
-}
+	}
 
-Mat matrix_alloc(size_t rows, size_t cols)
-{
+Mat matrix_alloc(size_t rows, size_t cols) {
 	Mat m;
 	m.cols = cols;
-	m.rows = rows; 
-	m.elem = (TYPE *)MATRIX_CALLOC(rows*cols,sizeof(*m.elem));    //(*M.elem) derefrence Changing type of element
+	m.rows = rows;
+	m.elem = (TYPE *)MATRIX_CALLOC((rows)*(cols)+1,sizeof(*m.elem));    //(*M.elem) derefrence Changing type of element
 	MATRIX_ASSERT(m.elem!=NULL);
 	return m;
-}
-void matrix_dot(Mat dest, Mat a, Mat b)  //a[1x2] b[2x3] c[1x3]
-{
-    MATRIX_ASSERT(a.cols == b.rows);
+	}
+void matrix_dot(Mat dest, Mat a, Mat b) { //a[1x2] b[2x3] c[1x3]
+	MATRIX_ASSERT(a.cols == b.rows);
 	MATRIX_ASSERT(dest.rows == a.rows);
 	MATRIX_ASSERT(dest.cols == b.cols);
-	for(size_t y = 0; y < dest.rows; y++){
-		for(size_t x = 0; x < dest.rows; x++){
-		    MATRIX_SHIFT(dest,y,x) = 0;
-			for(size_t i = 0;i < a.cols; i++){
+	for(size_t y = 0; y < dest.rows; y++) {
+		for(size_t x = 0; x < dest.cols; x++) {
+			MATRIX_SHIFT(dest,y,x) = 0;
+			for(size_t i = 0; i < a.cols; i++) {
 				MATRIX_SHIFT(dest,y,x) += MATRIX_SHIFT(a,y,i) * MATRIX_SHIFT(b,i,x);
+				}
+
 			}
-			
 		}
+
 	}
-	
-}
-void matrix_sum(Mat dest, Mat a)
-{
+void matrix_sum(Mat dest, Mat a) {
 	MATRIX_ASSERT(dest.cols == a.cols);
 	MATRIX_ASSERT(dest.rows == a.rows);
-	for(size_t y = 0; y < dest.rows;y++){
-		for(size_t x = 0; x < dest.rows;x++){
-	     
-		 MATRIX_SHIFT(dest,y,x) += MATRIX_SHIFT(a,y,x);  
+	for(size_t y = 0; y < dest.rows; y++) {
+		for(size_t x = 0; x < dest.cols; x++) {
+
+			MATRIX_SHIFT(dest,y,x) += MATRIX_SHIFT(a,y,x);
+			}
+		}
+
+	}
+void matrix_rand(Mat dest, float high,float low) {
+	for(size_t y = 0; y < dest.rows; y++) {
+		for(size_t x = 0; x < dest.cols; x++) {
+			MATRIX_SHIFT(dest,y,x) = (TYPE)((rand_float()*(high - low)) + low);   //IN RANGE
+			}
 		}
 	}
-	
-}
-void matrix_rand(Mat dest, float high,float low)
-{
-	for(size_t y = 0;y < dest.rows; y++){
-		for(size_t x = 0;x < dest.cols; x++){
-          MATRIX_SHIFT(dest,y,x) = (TYPE)((rand_float()*(high - low)) + low);   //IN RANGE
-		}
-}
-}
 
-void matrix_print(Mat m, const char *name)
-{
+void matrix_print(Mat m, const char *name) {
+	//system("pause");
 	printf("\n\n______________________________________________________________________________\n");
 	printf("%s",name);
 	printf(" = \n[\n");
-	for(size_t y = 0;y < m.rows; y++){
-		for(size_t x = 0;x < m.cols; x++){
-		   printf(" %.2f ", (float)MATRIX_SHIFT(m,y,x));
-		}
+
+	for(size_t y = 0; y < m.rows; y++) {
+		for(size_t x = 0; x < m.cols; x++) {
+			printf(" %.2f ", (float)MATRIX_SHIFT(m,y,x));
+			}
 		printf("\n\n");
-	}
+		}
 	printf("]\n");
 	printf("______________________________________________________________________________\n");
-	
-}
 
+	}
+
+float sigmoid(float x) {
+	return 1.0f/(1 + expf(-x));
+	}
+
+void matrix_activation_cols(Mat m,size_t num) {
+
+	for(size_t y = 0; y < m.rows; y++) {
+		MATRIX_SHIFT(m,y,num) = sigmoid(MATRIX_SHIFT(m,y,num));
+		}
+
+
+	}
+
+void matrix_activation(Mat m) {
+
+	for(size_t y = 0; y < m.rows; y++)
+		for(size_t x = 0; x < m.cols; x++)
+			MATRIX_SHIFT(m,y,x) = sigmoid(MATRIX_SHIFT(m,y,x));
+
+
+	}
+
+Mat matrix_as_col(Mat m, size_t num_of_cols) {
+	return (Mat) {
+		.cols = 1,
+		.rows = m.rows,
+		.elem = m.elem,
+
+		};
+	}
+//REWRITE IN ONE LOOP
+void matrix_feedforward(Mat *out,Mat input, Mat *weights, Mat *bias,size_t num_of_layer) {
+	matrix_dot(out[0],input,weights[0]);
+	for(size_t i = 1; i < num_of_layer; i++) {
+		//system("pause");
+		matrix_dot(out[i],out[i-1],weights[i]);
+		matrix_sum(out[i],bias[i]);
+		matrix_activation(out[i]);
+		}
+	}
+//*/
 
 
 #endif  //MATRIX_IMPLEMENTATION
