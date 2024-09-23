5afb39075278dfac70ed5b7c463a5d8d9368a352
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-09-23 19:48:40 +0200

    Update etherent.c

--- ESP32/main/etherent.c
+++ ESP32/main/etherent.c
@@ -1,14 +1,25 @@
 //#define ETHERNET_IMPLEMENTATION
-#define ETHERNET_IMPLEMENTATNION
+
+
+//#define ETHERNET_IMPLEMENTATNION
 #include "defs.h"
-#include "ethernet.h"
-#define SERVER_IMPLEMENTATION
-#include "server.h"
+//#include "ethernet.h"
+//#define SERVER_IMPLEMENTATION
+//#include "server.h"
+#define CLIENT_IMPLEMNTATION
+#include "client.h"
+//#define WIFI_IMPLEMENTATION
+#define WIFI_IMPLEMENTATION
+
+#include "wifi.h"
 
 void app_main(){
-    ethernet_init();
-    BLOCK_TASK(ETHERNET, 100);
+    //ethernet_init();
+    wifi_init();
+    //BLOCK_TASK(ETHERNET, 100);
+    BLOCK_TASK(WIFI, 100);
     ESP_LOGI("main", "This is logeded last\n\n");
-    server_init();
-
+    //server_init();
+    rest_get("http://8.8.8.8");
+    rest_post("http://8.8.8.8");
 }
