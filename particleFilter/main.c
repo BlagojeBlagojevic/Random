66a95e3a0c963008f9ad03f600f80c3fdc7df1dd
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-09-17 10:59:47 +0200

    Update main.c

--- particleFilter/main.c
+++ particleFilter/main.c
@@ -14,7 +14,7 @@
 #define DROP(var) {(void)var;}
 
 
-#define DA_SIZE 16
+#define DA_SIZE 64
 #define da_append(da, item)                                                            \
     do {                                                                                 \
         if ((da)->count >= (da)->capacity) {                                               \
@@ -107,42 +107,42 @@ static inline float model_rssi(ParticleFilter *pf, float d){
     return pf->A - 10.0f * pf->n * log10f(d);
 }
 
-static inline void step_PF(ParticleFilter *pf, float rssi_measured){
+void step_PF(ParticleFilter *pf, float rssi_measured){
     float w_sum = 0.0f;
-
     for(size_t i=0; i<pf->p.count; i++){
         // prediction
         float noise = pf->sig_pro * rand_GAUS(pf->s);
         pf->p.items[i].d += noise;
         CLAMP(pf->p.items[i].d, pf->d_min, pf->d_max);
-
         float rssi_pred = model_rssi(pf, pf->p.items[i].d);
-
         float diff = rssi_measured - rssi_pred;
         float w = expf(-0.5f * (diff*diff)/(pf->sig_meas*pf->sig_meas));
         pf->p.items[i].w = w;
         w_sum += w;
     }
-
+    
     for(size_t i=0; i<pf->p.count; i++){
         pf->p.items[i].w /= w_sum;
     }
+   
     //
     Particle_DA newp = {0};
     float step = 1.0f / pf->p.count;
     float r = rand_GAUS(pf->s) * step;
     float c = pf->p.items[0].w;
     size_t i = 0;
+    
     for(size_t m=0; m<pf->p.count; m++){
         float U = r + m*step;
-        while(U > c){
+        while(U > c && i < pf->p.count - 1){
             i++;
             c += pf->p.items[i].w;
         }
         da_append(&newp, pf->p.items[i]);
         newp.items[m].w = 1.0f/pf->p.count;
     }
-    //free(pf->p.items);
+    //exit(-1);
+    free(pf->p.items);
     pf->p = newp;
 }
 
@@ -178,18 +178,33 @@ int main(){
      da_append(&rssi_stream, rssi);            
     }
     
-    FILE *f = fopen("temp.txt", "w+");
+    FILE *f = fopen("temp.py", "w+");
+    fprintf(f, "import numpy as np\n");
+    fprintf(f, "import matplotlib.pyplot as plt\n");
+    fprintf(f, "dist = [");
+    for(size_t i = 0; i < rssi_stream.count; i++){
+    	fprintf(f, "%f, ", distanc[i]);
+    }
+    fprintf(f, "]\n");
+    fprintf(f, "distParticle = [ ");
+    
     for(int t=0; t<distSize; t++){
-        step_PF(pf, rssi_stream.items[t]);
-        step_PF(pf, rssi_stream.items[t]);
-        step_PF(pf, rssi_stream.items[t]);
-        step_PF(pf, rssi_stream.items[t]);
+//        step_PF(pf, rssi_stream.items[t]);
+//        step_PF(pf, rssi_stream.items[t]);
+//        step_PF(pf, rssi_stream.items[t]);
+//        step_PF(pf, rssi_stream.items[t]);
         step_PF(pf, rssi_stream.items[t]);
         float est = estimate_PF(pf);
-        fprintf(f, "Step %d: RSSI=%.1f -> Estimated distance=%.3f m\n", t, rssi_stream.items[t], est);
-        //fprintf(f, "%f, ", est);
+        fprintf(f, "%f, ", est);
+        //fprintf(f, "Step %d: RSSI=%.1f -> Estimated distance=%.3f m\n", t, rssi_stream.items[t], est);
     }
-
+    fprintf(f, "]\nplt.plot(dist)\nplt.plot(distParticle)\nplt.savefig(\"comp.png\")\nplt.show()");
+    fclose(f);
+    //sleep(10);
+    system("python temp.py");
+    //exit(-1);
+    //int a = 0;
+    //fread(&a, 1, sizeof(int), stdin);
     LOG("\n --------------------------------------------------- \n");
     return 0;
 }
